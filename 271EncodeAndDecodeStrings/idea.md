ca27e0820dbee59663ac660471966427c4300e3f
devhaocui <devhaocui@gmail.com>
2025-03-21 22:38:58 -0700

    testing image display

---
+++ 271EncodeAndDecodeStrings/idea.md
@@ -0,0 +1,9 @@
+# Idea.md
+
+## Problem
+
+![Problem](https://imgur.com/a/vjlt5MU)
+- Design an algorithm to encode a list of strings to a single string. The encoded string is then decoded back to the original list of strings.
+
+## Solution
+- Solution Description
---
+++ 271EncodeAndDecodeStrings/solution.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+class Solution {
+public:
+  bool someFunction() {
+    // code here
+    return false;
+  }
+};
+
+int main() {
+  std::cout << "Running code\n";
+
+  std::vector<int> inputVector = {1, 2, 2, 3, 4};
+  //Solution o1;
+  //o1.someFunction();
+  return 0;
+}
